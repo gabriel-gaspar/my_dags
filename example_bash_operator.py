091faf6b5b5e1bbcb3e62f44c3e22674a4958b14
gabriel-gaspar <gabrielgaspar@wine.com.br>
2021-09-14 16:36:47 -0300

    add code to submit spark-pi job

--- example_bash_operator.py
+++ example_bash_operator.py
@@ -1,74 +1,42 @@
-# -*- coding: utf-8 -*-
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-
-from builtins import range
-from datetime import timedelta
-
-from airflow.models import DAG
+from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.utils.dates import days_ago
+from datetime import datetime, timedelta
 
-args = {
-    'owner': 'Airflow',
-    'start_date': days_ago(2),
+default_args = {
+    'owner': 'gaspar',
+    'start_date': datetime(2021, 9, 11)
 }
 
-dag = DAG(
-    dag_id='example_bash_operator',
-    default_args=args,
-    schedule_interval='0 0 * * *',
-    dagrun_timeout=timedelta(minutes=60),
-    tags=['example']
-)
+with DAG(
+    'spark-pi-job',
+    default_args=default_args,
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+    tags=['example'],
+) as dag:
 
-run_this_last = DummyOperator(
-    task_id='run_this_last',
-    dag=dag,
-)
 
-# [START howto_operator_bash]
-run_this = BashOperator(
-    task_id='run_after_loop',
-    bash_command='echo 1',
-    dag=dag,
-)
-# [END howto_operator_bash]
-
-run_this >> run_this_last
-
-for i in range(3):
-    task = BashOperator(
-        task_id='runme_' + str(i),
-        bash_command='echo "{{ task_instance_key_str }}" && sleep 1',
-        dag=dag,
+    t1 = BashOperator(
+        task_id='spark_pi_job',
+        bash_command="""/opt/airflow/spark-3.1.2-bin-hadoop3.2/bin/spark-submit \
+                        --class org.apache.spark.examples.SparkPi \
+                        --master k8s://kubernetes.default.svc \
+                        --deploy-mode cluster \
+                        --driver-cores 1 \
+                        --driver-memory 512m --executor-cores 1 \
+                        --executor-memory 512m --num-executors 1 \
+                        --conf spark.kubernetes.namespace=spark \
+                        --conf spark.kubernetes.container.image=gcr.io/spark-operator/spark:v3.1.1 \
+                        --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark-operator-spark \
+                        --conf spark.kubernetes.container.image.pullPolicy=IfNotPresent \
+                        --conf spark.kubernetes.driver.podTemplateFile=/opt/airflow/job_templates/basic-template.yaml \
+                        --conf spark.kubernetes.executor.podTemplateFile=/opt/airflow/job_templates/basic-template.yaml \
+                        local:///opt/spark/examples/jars/spark-examples_2.12-3.1.1.jar 1000""",
     )
-    task >> run_this
 
-# [START howto_operator_bash_template]
-also_run_this = BashOperator(
-    task_id='also_run_this',
-    bash_command='echo "run_id={{ run_id }} | dag_run={{ dag_run }}"',
-    dag=dag,
-)
-# [END howto_operator_bash_template]
-also_run_this >> run_this_last
+    t2 = BashOperator(
+        task_id="end_message",
+        bash_command='echo "The task is done!"'
+    )
 
-if __name__ == "__main__":
-    dag.cli()
+    t1 >> t2
